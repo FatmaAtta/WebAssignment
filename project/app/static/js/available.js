7bb9199cc13917f6227fe30d5fe008391500767c
Fatma Atta <87873165+FatmaAtta@users.noreply.github.com>
2024-05-18 02:13:06 +0300

    main django app

--- project/app/static/js/available.js
+++ project/app/static/js/available.js
@@ -61,38 +61,3 @@
 
                     }
         }
-    // });
-
-// });
-        
-        
-        // const borrowButton = book.querySelector('.borrowBook');
-        // // console.log(borrowButton);
-        //     borrowButton.addEventListener('click', function(event) {
-        //         // Handle borrow button click
-        //         book.isAvailable=false;
-
-        //         localStorage.setItem("Books", JSON.stringify(books));
-        //         console.log("Borrow button clicked for book:", book);
-
-        //         borrow(book);
-               
-               
-        //         console.log(document.getElementById('borrowedList'));
-        //         const bBook=document.createElement('div');
-        //         bBook.classList.add('book');
-        //         bBook.innerHTML=`
-        //         <a href="book_details.html?${sendQuery.toString()}" target="_blank">
-        //         <div class="hiddenDetails">
-        //         <p class="title">${title}</p>
-        //         <p class="author">${author}</p>
-        //         <p class="category">${category}</p>
-        //         <p class="description">${description}</p>
-        //         </div>
-        //         <img src="${img}" alt="book image">
-        //         </a>
-        //         <button class="returnBook">Return</button>
-        //         `;
-
-        //         // document.getElementById('borrowedList').appendChild(bBook);
-        // });
--- project/app/views.py
+++ project/app/views.py
@@ -1,8 +1,5 @@
 from django.shortcuts import render, redirect
-from .models import User, Book, Category
-from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
-from django.contrib.auth.hashers import make_password
-from django.contrib.auth import authenticate, login
+from .models import Book
 from .forms import BookForm
 from django.contrib import messages
 
@@ -10,49 +7,49 @@ def main(request):
     return render(request,'main.html')
 
 def second_main(request):
-    return render(request, 'second_main.html')
+    active_tab = 'second_main'
+    recommended = Book.objects.all()[:7]
+    best = Book.objects.all()[:5]
+    return render(request, 'second_main.html', {'active_tab': active_tab, 'recommended':recommended, 'best': best})
 
 def available_books(request):
+    letter = request.user.username[0].upper()
+    active_tab = 'available'
     books = Book.objects.all()
-    return render(request, 'availableBooks.html', {'books':books})
+    return render(request, 'availableBooks.html', {'books':books, 'active_tab': active_tab, 'letter':letter})
 
 
 def borrowed_books(request):
-    return render(request, 'borrowed.html')
+    letter = request.user.username[0].upper()
+    active_tab = 'borrowed'
+    return render(request, 'borrowed.html', {'active_tab': active_tab, 'letter':letter})
 
 def book_details(request):
     return render(request, 'book_details.html')
 
-def sign_in(request):
-    return render(request, 'signin_page.html')
-
 def add_book(request):
+    active_tab = 'add_book'
     if request.method=="POST":
         form=BookForm(request.POST, request.FILES) #must add request.FILES when your form contains image input
         if form.is_valid():
             form.save()
             messages.success(request, "Book added successfully!", extra_tags= 'book_added')
             form = BookForm() #to clear the form fields
-            # return redirect('available_books')
+            return redirect('add_book')
         else:
             print(form.errors)
     else:
         form = BookForm()
-    context={'form':form}
-    return render(request, 'add_book.html', context)
-
-
-
+        letter = request.user.username[0].upper() if request.user.username else ' '
+    return render(request, 'add_book.html', {'form': form, 'active_tab': active_tab, 'letter':letter})
 
 def edit_details(request):
     return render(request, 'admin_details.html')
 
 def admin_page(request):
+    active_tab = 'admin_page'
+    letter = request.user.username[0].upper()
     books = Book.objects.all()
-    return render(request, 'admin.html', {'books':books})
+    return render(request, 'admin.html', {'books':books, 'active_tab':active_tab, 'letter':letter})
 
-def sign_up(request):
-    return render(request, 'signup_page.html')
 
-def sign_in(request):
-    return render(request, 'signin_page.html')
